226277d448b40a073308bfacb39777481cedcc2f
Eyüp Güzel <eyupguzel1253@gmail.com>
2024-04-26 12:12:08 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace patika.dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Eyup";
+            string surname = " Guzel";
+            Console.WriteLine(name + surname);
+
+            int age = 22;
+            Console.WriteLine(age);
+
+            float temperature = 25.86f;
+            Console.WriteLine(temperature);
+
+            double area = 5.2 * 3.4;
+            Console.WriteLine(area);
+
+            bool tf = 19 < 25;
+            Console.WriteLine(tf);
+
+            object person = new object();
+            person = name + surname;
+
+            Console.WriteLine(person);
+
+            string a1 = "20";
+            int a2 = 24;
+            Console.WriteLine(a1 + a2.ToString());
+
+        }
+
+
+
+
+
+       
+
+
+}
+}
+ 
